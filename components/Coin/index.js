123a7486b95b4d6f5533762bfb1893f20853f81c
Aslam Sarfraz <aslam91r@gmail.com>
2021-04-18 01:22:22 +0500

    Get Data from API

--- components/Coin/index.js
+++ components/Coin/index.js
@@ -1,15 +1,15 @@
 import styles from './Coin.module.css'
-export default function Coin(){
+export default function Coin({CoinData}){
     return(
         <div className={styles.CoinBody}>
             <div className={styles.CoinICON}></div>
-            <div className={styles.CoinSYB}>BTC</div>
-            <div className={styles.CoinNAME}>Bitcoin</div>
-            <div className={styles.CoinPRICE}>55,000 USD</div>
+            <div className={styles.CoinSYB}>{CoinData.symbol}</div>
+            <div className={styles.CoinNAME}>{CoinData.name}</div>
+            <div className={styles.CoinPRICE}>{CoinData.current_price} USD</div>
             <div className={styles.Coin24DG}>4.37%</div>
             <div className={styles.Coin7DG}>2.77%</div>
-            <div className={styles.CoinMC}>$127,169,230,004,12</div>
-            <div className={styles.CoinSER}>$198,668,989,668</div>
+            <div className={styles.CoinMC}>${CoinData.market_cap}</div>
+            <div className={styles.CoinSER}>${CoinData.circulating_supply}</div>
         </div>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,10 @@ import Footer from '../components/Footer'
 import Coin from '../components/Coin'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function Home({ result }) {
+
+  console.log(result);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -15,14 +18,45 @@ export default function Home() {
       <MainHeader />
       <StockLine />
       <div className={styles.MainBody}>
-        <Coin />
-        <Coin />
-        <Coin />
-        <Coin />
-        <Coin />
-        <Coin />
+        {
+          result.map((coin) => {
+            return (
+              <Coin CoinData={coin} />
+            )
+          })
+        }
       </div>
-      <Footer/>
+      <Footer />
     </div>
   )
 }
+
+export const getServerSideProps = async (context) => {
+
+  const res = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false');
+  const result = await res.json();
+
+  if (!result) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    }
+  }
+
+  return {
+    props: {
+      result
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
